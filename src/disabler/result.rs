d474c4c7769715bab357ccce1c3e65c6973d339c
William Tremblay <tremwil@gmail.com>
2025-08-27 00:27:55 +0100

    support delayed load

--- src/disabler/result.rs
+++ src/disabler/result.rs
@@ -1,4 +1,4 @@
-use std::{any::Any, fmt};
+use std::{any::Any, fmt, panic::UnwindSafe};
 
 pub type DearxanResult = std::result::Result<Status, Error>;
 
@@ -18,20 +18,25 @@ pub enum Error {
     Panic(String),
 }
 
-pub(crate) fn from_error<E: Into<Box<dyn std::error::Error + Send + Sync>>>(
-    err: E,
-) -> DearxanResult {
-    Err(Error::Error(err.into()))
+impl Error {
+    pub(crate) fn from_panic_payload(payload: Box<dyn Any + Send + 'static>) -> Self {
+        // As of Rust 2024, library panic payloads are always `&'static str`.
+        match payload.downcast::<&'static str>() {
+            Ok(panic_msg) => Self::Panic(panic_msg.to_string()),
+            Err(_) => Self::Panic("panicked, but failed to retrieve panic message".to_owned()),
+        }
+    }
 }
 
-pub(crate) fn from_panic_payload(payload: Box<dyn Any + Send + 'static>) -> DearxanResult {
-    // As of Rust 2024, library panic payloads are always `&'static str`.
-    match payload.downcast::<&'static str>() {
-        Ok(panic_msg) => Err(Error::Panic(panic_msg.to_string())),
-        Err(_) => Err(Error::Panic(
-            "panicked, but failed to retrieve panic message".to_owned(),
-        )),
-    }
+pub(crate) fn from_maybe_panic<T, E>(
+    fun: impl FnOnce() -> Result<T, E> + UnwindSafe,
+) -> Result<T, Error>
+where
+    E: Into<Box<dyn std::error::Error + Send + Sync>>,
+{
+    std::panic::catch_unwind(fun)
+        .map(|res| res.map_err(|e| Error::Error(e.into())))
+        .unwrap_or_else(|e| Err(Error::from_panic_payload(e)))
 }
 
 impl fmt::Display for Error {
